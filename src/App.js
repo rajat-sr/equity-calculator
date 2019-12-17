8c361cde459574477d4f15eb817a088e991415b0
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-12-17 20:09:43 +0530

    feat: Create basic layout using Material UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import classes from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className={classes.Main}>
+        <div className={classes.Card}>
+          <Typography variant="h3">Equity Calculator</Typography>
+          <TextField
+            id="total-shares"
+            label="Total Shares in the Company"
+            className={classes.TextInput}
+            margin="normal"
+          />
+          <TextField
+            id="company-valuation"
+            label="Valuation of the Company"
+            className={classes.TextInput}
+            margin="normal"
+            InputProps={{
+              startAdornment: <InputAdornment position="start">$</InputAdornment>,
+            }}
+          />
+          <TextField
+            id="your-shares"
+            label="Number of shares you hold"
+            className={classes.TextInput}
+            margin="normal"
+          />
+          <div className={classes.Result}>
+            <Typography>YOUR SHARES ARE WORTH</Typography>
+            <Typography variant="h4">$89765</Typography>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
